4eb12ae4505b5cd8c01a678aa7c33ff3f341b8ca
LeGrosLezard <53193299+LeGrosLezard@users.noreply.github.com>
2019-08-03 19:56:15 +0200

    Add files via upload
    bon j'ai l'oeil mais... que jmatte en haut (dailleurs ca fait mal) ou normale ben justement normal le rond est plus haut... faut trouvé un truk et mes conteneurs d'oeil ? sont en amande aussi...

--- yeux/clignement/essais_image.py
+++ yeux/clignement/essais_image.py
@@ -1,22 +1,45 @@
 import numpy as np
 import cv2 
+from PIL import Image
+import os
 
 
-left_eye = cv2.CascadeClassifier('haarcascade_lefteye_2splits.xml')
-img = cv2.imread('test.jpg')
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+img = Image.open('image61.jpg')
+img = img.resize((300, 300), Image.ANTIALIAS)
+img.save("iiiiiii.jpg")
 
 
-eyes = left_eye.detectMultiScale(gray)
+img = cv2.imread('iiiiiii.jpg', 0)
+blur = cv2.blur(img,(3,3))
 
-c = 0
-for (ex, ey, ew, eh) in eyes:
-    if c % 2 == 0:
+
+circles = cv2.HoughCircles(img, cv2.HOUGH_GRADIENT, 2, 100, param1=50,
+                            param2=30, minRadius=40, maxRadius=55)
+
+circles = np.round(circles[0, :]).astype("int")
+
+for (x, y, r) in circles:
+        cv2.circle(img, (x, y), r, (0, 255, 0), 2)
+        cv2.circle(img, (x, y), 2, (0, 0, 255), 3)
+
+cv2.imwrite("yo1.jpg", img)
+cv2.imshow("output", img)
+
+cv2.waitKey(0)
+
+
+
+
+
+
+
+liste = os.listdir()
+
+for i in liste:
+    if i == "essais_image.py":
         pass
     else:
-        cv2.rectangle(gray, (ex,ey), (ex+ew, ey+eh), (0, 255, 0), 2)
-        crop_img = gray[ey:ey+eh, ex:ex+ew]
-    c+=1
+        print(i)
 
 
 
@@ -24,5 +47,17 @@ for (ex, ey, ew, eh) in eyes:
 
 
 
-    
-cv2.imshow('LEFT', crop_img)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
