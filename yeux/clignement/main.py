0d9b3c28e053356903ba2de4e43dee6a0e75d823
LeGrosLezard <53193299+LeGrosLezard@users.noreply.github.com>
2019-08-08 00:49:31 +0200

    Update main.py
    ok prendre lhaar avec les lunettes ca me fatigue...

--- yeux/clignement/main.py
+++ yeux/clignement/main.py
@@ -2,187 +2,56 @@ import numpy as np
 import cv2
 from PIL import Image
 import os
-from statistics import mean
 
-from CONFIG import PATH_Y_G
-from CONFIG import PATH_Y_D
-
-from traitement_image import position_yeux
-
-
-
-def video_oeil_gauche(video):
-    #Main frame
-
-    try:
-        
-        im = cv2.imread("gauche_oeil.jpg")
-        cv2.imshow('gauche_oeil', im)
- 
-    except:
-        pass
-
-def video_oeil_droit(video):
-    #Main frame
-
-    try:
-        
-        im1 = cv2.imread("droite_oeil.jpg")
-        cv2.imshow('droite_oeil', im1)
-
- 
-    except:
-        pass
-
-    
-def video_left(video):
-    #Eyes left
-
-
-    left_eye = cv2.CascadeClassifier('haarcascade_lefteye_2splits.xml')
-    
-  
-    ret, frame = video.read()
-    left = cv2.resize(frame, (400, 400))
-    eyes = left_eye.detectMultiScale(left)
-    
-    c = 0
-    for (ex, ey, ew, eh) in eyes:
-        if c % 2 == 0:
-            pass
-        else:
-            cv2.rectangle(left, (ex,ey), (ex+ew, ey+eh), 0)
-            left = left[ey:ey+eh, ex:ex+ew]
-            
-            cv2.imwrite(PATH_Y_G, left)
-
-          
-            position_yeux("gauche_oeil.jpg", "iiiiiii.jpg", "image_position_yeux_gauche.jpg")
-
-            
-        c += 1
-
-    
-    try:
-        cv2.imshow('LEFT', left)
-    except:
-        pass
-
-
-liste = []
-def yo(video, repere):
-    #Eyes left
-
-    
-    left_eye = cv2.CascadeClassifier('haarcascade_lefteye_2splits.xml')
-    
-      
-    ret, frame = video.read()
-    left = cv2.resize(frame, (400, 400))
-    eyes = left_eye.detectMultiScale(left)
-
-    if repere == 100:
-        print("feazKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKK")
-    
-    if repere > 100:
-        
-        for (ex, ey, ew, eh) in eyes:
-
-            cv2.rectangle(left, (ex,ey), (ex+ew, ey+eh), 0)
-            liste.append(ey)
-            
-            if ey < sum(liste)/len(liste) - 5:
-                print("HAUT")
-            if ey > sum(liste)/len(liste) + 5: 
-                print("bas")
-        try:
-            
-            cv2.imshow('LEFT', left)
-        except:
-            pass
-
-
-    if repere < 100:
-        for (ex, ey, ew, eh) in eyes:
-            
-            cv2.rectangle(left, (ex,ey), (ex+ew, ey+eh), 0)
-
-            ey = ey.tolist()
-
-            liste.append(ey)
-
-            
-            
-        try:
-     
-            cv2.imshow('LEFT', left)
-        except:
-            pass
-
-
-def video_right(video):
-    #Eyes right
-
- 
-    right_eye = cv2.CascadeClassifier("haarcascade_lefteye_2splits.xml")
-    
-    ret, frame = video.read()
-    right = cv2.resize(frame, (400, 400))
-    eyes = right_eye.detectMultiScale(right)
-
-    c = 0
-    for (ex, ey, ew, eh) in eyes:
-        
-        if c % 2 == 0:
-            cv2.rectangle(right, (ex,ey), (ex+ew, ey+eh), 0)
-            right = right[ey:ey+eh, ex:ex+ew]
-            
-            cv2.imwrite(PATH_Y_D, right)
-
-            try:
-                position_yeux("droite_oeil.jpg", "jjj.jpg", "image_position_yeux_droit.jpg")
-            except:
-                pass
-
-        else:
-            pass
-    
-        c += 1
-
-    
-
-    try:
-        cv2.imshow('RIGHT', right)
-    except:
-        pass
-    
+from traitement_image import *
 
 
 
 
 
+
+
+
+LISTE_AJUSTEMENT = []
 def video_capture():
 
-    GLOBAL = []
+    LISTE_AJUSTEMENT = []
     video = cv2.VideoCapture(0)
     repere = 0
+    
     while(True):
-        if repere % 10 == 0:
-            print("REPEREEEEEEEEEEEEEEEE")
-        if repere % 30 == 0:
 
-            liste = ["iiiiiii.jpg", "jjj.jpg", "droite_oeil.jpg", "gauche_oeil.jpg"]
-            for i in liste:
-                os.remove(i)
-            
-                new_im = Image.new('RGB', (300,300), (255,255,255))
-                new_im.save(i, "jpeg")
 
-        yo(video, repere)
-        #video_oeil_gauche(video)
-        #video_oeil_droit(video)
-        #video_left(video)
-        #video_right(video)
+        left_eye = cv2.CascadeClassifier('haarcascade_lefteye_2splits.xml')
+        
+          
+        ret, frame = video.read()
+        frame = cv2.resize(frame, (600, 600))
+        eyes = left_eye.detectMultiScale(frame)
+
+
+        if len(LISTE_AJUSTEMENT) < 100:
+            pre_initialisation(repere, eyes, LISTE_AJUSTEMENT, frame)
+            print("initialisation")
+    
+        else:
+            position = position_yeux(repere, eyes, LISTE_AJUSTEMENT, frame)
+            if position == None:
+                pass
+            else:
+                print(position)
+
+            if position in ("le mec s'est baissé", "le mec s'est levé"):
+                LISTE_AJUSTEMENT = []
+
+
+
+
+        cv2.imshow('YEUX CAPTURE', frame)
+
+
+
+
 
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
