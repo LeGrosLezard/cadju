0b1cfcc2b90d88ecea19adc87dbcc2a476b756da
LeGrosLezard <53193299+LeGrosLezard@users.noreply.github.com>
2019-08-08 00:48:40 +0200

    Update CONFIG.py

--- yeux/clignement/CONFIG.py
+++ yeux/clignement/CONFIG.py
@@ -1,3 +1,22 @@
-PATH_HAAR = r"C:\Users\jeanbaptiste\Desktop\cadju\yeux\clignement\haarcascade\{0}"
-EYES_RIGHT = PATH_HAAR.format("haarcascade_lefteye_2splits.xml")
-EYES_LEFT = PATH_HAAR.format("haarcascade_righteye_2splits.xml")
+dico = {"le mec à levé la tete":"signe de défi ou a vu un truk",
+        "le mec à baisser la tete":"signe de soumission ou ramasse un truk",
+        "le mec regarde en HAUT":"recherche active d'information en mémoire ou a vu un truk",
+        "le mec regarde en bas":"imagination ou il a vu un truk ou soumission",
+        "le mec s'est baissé":"comme si un sac le pesé",
+        "le mec s'est levé":"comme quand tu va a la mer et que t'as un peu de bide",
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
