ab99e00cae2bfd373b03f499ff1803c677b4afc4
LeGrosLezard <53193299+LeGrosLezard@users.noreply.github.com>
2019-08-08 21:50:56 +0200

    Update CONFIG.py
    dingue ca !!! en vrai

--- yeux/clignement/CONFIG.py
+++ yeux/clignement/CONFIG.py
@@ -4,7 +4,12 @@ dico = {"le mec à levé la tete":"signe de défi ou a vu un truk",
         "le mec regarde en bas":"imagination ou il a vu un truk ou soumission",
         "le mec s'est baissé":"comme si un sac le pesé",
         "le mec s'est levé":"comme quand tu va a la mer et que t'as un peu de bide",
-
+        "le mec a regarder en haut a gauche":"imagination image",
+        "le mec a gauche":"imagination son",
+        "le mec a regarder en bas a gauche":"verification sentiment",
+        "le mec a regarder en haut a gauche":"voisualisation image",
+        "le mec a regarder a droite":"mémoire son",
+        "le mec a regarder en bas a droite":"dialogue interne",
 
 
 
