0af23ddece69a98fc310ed9afe7593537f85a82c
LeGrosLezard <53193299+LeGrosLezard@users.noreply.github.com>
2019-08-08 18:42:30 +0200

    Update traitement_image.py
    ok ca fait mal au yeux du coup je bouge la tete... 
    DU COUP faut mettre - 3 mais si supp a 5 NON c la tete okkkkk ! bon c bon moi jdis que ca marche (rajoute le gauche en rouge en vrai faudra tout enlevé OU mettre tout ca en caché )

--- yeux/clignement/traitement_image.py
+++ yeux/clignement/traitement_image.py
@@ -6,9 +6,29 @@ import os
 
 
 
-def pre_initialisation(eyes, liste, frame):
+def pre_initialisation(eyes, liste, frame, liste1):
     """Ici si repere est inférieur a 100
     on ajoute les lignes dans la liste"""
+
+    c = 0
+
+    if len(eyes) == 1:
+        pass
+    else:
+    
+        for (ex, ey, ew, eh) in eyes:
+
+            if c == 0:
+                cv2.rectangle(frame, (ex,ey), (ex+ew, ey+eh), 2)
+                cv2.circle(frame, (round(int(ex+(ew/2))),
+                                   round(int(ey+(eh/2+5)))), 3, (255, 255, 255), 5)
+                ex = ex.tolist()
+                ew = ew.tolist()
+                liste1.append(round(int(ex+(ew/2))))
+
+
+        
+
 
     for (ex, ey, ew, eh) in eyes:
 
@@ -26,13 +46,10 @@ def position_yeux_verticale(eyes, liste, frame):
     """Si repere est superieur a 100
     c'est qu'on a fini la premiere initialisation"""
 
-
+    
     for (ex, ey, ew, eh) in eyes:
 
         cv2.rectangle(frame, (ex,ey), (ex+ew, ey+eh), 2)
-        cv2.circle(frame, (round(int(ex+(ew/2))), round(int(ey+(eh/2+5)))), 3, (0, 0, 255), 5)
-        
-        ey = ey.tolist()
 
         if ey < sum(liste)/len(liste) - 90:
             return "le mec s'est levé"
@@ -59,13 +76,45 @@ def position_yeux_verticale(eyes, liste, frame):
 
 
 
-        
+
+
+def position_yeux_horizontal(eyes, liste, liste1, frame):
+
+    c = 0
+
+    if len(eyes) == 1:
+        pass
+    else:
+    
+        for (ex, ey, ew, eh) in eyes:
+
+            if c == 0:
+                cv2.rectangle(frame, (ex,ey), (ex+ew, ey+eh), 2)
+                
+                cv2.circle(frame, (round(int(ex+(ew/2))),
+                                   round(int(ey+(eh/2+5)))), 3, (0, 0, 255), 5)
+
+                ex = ex.tolist()
+                ew = ew.tolist()
+                liste1.append(round(int(ex+(ew/2))))
+
+                try:
+
+                    #print("liste",liste1[-2])
+                    #print(int(ex+(ew/2)))
+                    
+                    if round(int(ex+(ew/2))) < liste1[-2] - 3:
+                        print("gauche")
+
+                    elif round(int(ex+(ew/2))) > liste1[-2] + 3:
+                       print("droite")
+                    
+                except:
+                    pass
 
 
 
-
-
-
+            c+=1
 
 
 
