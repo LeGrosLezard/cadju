7be1afab7f70a914631d49d789154b1564be1ba7
LeGrosLezard <53193299+LeGrosLezard@users.noreply.github.com>
2019-08-10 17:20:39 +0200

    Update traitement.py
    faut en faire une fonction et verifier pour le switch d'oeil
    
    pour eviter le bug faudra faire: des conditions faut pas que tout sois mis du genre si le mec se touche la figure, on enleve les yeux et tout ect et notre truk de base c les yeux
    
    normalment c bon MAIS le probleme c la longueur du nez... donc au pire rallie lange et le nez trouve un haar pour la bouche... mais le nez marche pas rtes bien ptetre que la bouche oui
    
    faire les zones de la figure mtn

--- tete/visage/traitement.py
+++ tete/visage/traitement.py
@@ -6,16 +6,24 @@ import os
 
 
 def point_figure(frame, x, y, w, h):
+    
     x = x.tolist()
     y = y.tolist()
     w = w.tolist()
     h = h.tolist()
     
-    tempe_droite = cv2.circle(frame,(x + 10, y + 10), 5, (255,0,0), 5)
-    tempe_gauche = cv2.circle(frame,(x + w - 10 , y + 10), 5, (255,0,0), 5)
+    tempe_gauche = cv2.circle(frame,(x + w - 10 , y + 10), 1, (255,0,0), 2)
+    tempe_droite = cv2.circle(frame,(x + 10, y + 10), 1, (255,0,0), 2)
+    
+    
+    oreille_gauche = cv2.circle(frame,(x + w , y + 50), 1, (255,0,0), 2)
+    oreille_droite = cv2.circle(frame,(x , y + 50), 1, (255,0,0), 2)
 
-    oreille_droite = cv2.circle(frame,(x , y + 60), 5, (255,0,0), 5)
-    oreille_gauche = cv2.circle(frame,(x + w , y + 60), 5, (255,0,0), 5)
+    cerne_droite = cv2.circle(frame, (x + 50, int(round(y + h / 2 - 5))), 1, (255,0,0), 2)
+    cerne_gauche = cv2.circle(frame, (x + w - 50, int(round(y + h / 2 - 5))), 1, (255,0,0), 2)
+
+    pomette_droite = cv2.circle(frame, (x + 40, int(round(y + h / 2 + 10))), 1, (255,0,0), 2)
+    pomette_gauche = cv2.circle(frame, (x + w - 40, int(round(y + h / 2 + 10))), 1, (255,0,0), 2)
 
 
 
@@ -41,6 +49,101 @@ def figure(frame, video, faceCascade, gray):
 
 def yeux(frame, video, eyesCascade):
     eyes = eyesCascade.detectMultiScale(frame)
+
+
+
+    c = 0
     
     for x1, y1, w1, h1 in eyes:
-        cv2.rectangle(frame, (x1,y1), (x1+w1, y1+h1),(0, 0, 255), 2)
+        
+        if c == 0:
+            pos1 = x1.tolist() + w1.tolist()
+            pos1_y = y1.tolist()
+        
+            sourcile6 = cv2.circle(frame, (x1 + 5, y1 + 5), 1, (0,0,0), 2)
+            sourcile7 = cv2.circle(frame, (x1 + 15, y1 + 5), 1, (0,0,0), 2)
+            sourcile8 = cv2.circle(frame, (x1 + 25, y1 + 5), 1, (0,0,0), 2)
+            sourcile9 = cv2.circle(frame, (x1 + 35, y1 + 6), 1, (0,0,0), 2)
+            sourcile10 = cv2.circle(frame, (x1 + 45, y1 + 7), 1, (0,0,0), 2)
+
+        elif c == 1:
+
+            pos2 = x1.tolist()
+            pos2_y = y1.tolist()
+            
+            #cv2.rectangle(frame, (x1, y1), (x1 + w1, y1 + h1),(0, 0, 255), 2)
+            sourcile1 = cv2.circle(frame, (x1 + 5, y1 + 5), 1, (0,0,0), 2)
+            sourcile2 = cv2.circle(frame, (x1 + 15, y1 + 5), 1, (0,0,0), 2)
+            sourcile3 = cv2.circle(frame, (x1 + 25, y1 + 5), 1, (0,0,0), 2)
+            sourcile4 = cv2.circle(frame, (x1 + 35, y1 + 6), 1, (0,0,0), 2)
+            sourcile5 = cv2.circle(frame, (x1 + 45, y1 + 7), 1, (0,0,0), 2)
+
+        c += 1
+
+        #ATTENTION A SWITCH DES YEUX
+
+
+    un = 0
+    deux = 0
+
+    if pos1 < pos2:
+        un = pos1
+        deux = pos2
+
+        unun = pos1_y
+        deuxdeux = pos2_y
+
+        x = (un + deux)/2
+        y = (unun + deuxdeux)/2
+        print(un)
+        
+    elif pos1 > pos2:
+        deux = pos1
+        un = pos2
+
+        unun = pos2_y
+        deuxdeux = pos1_y
+
+        x = (un + deux)/2
+        y = (unun + deuxdeux)/2
+        y = int(round(y))
+
+
+    entre_oeil = cv2.circle(img, (int(round(x)), int(round(y))), 1, (255,0,0), 2)
+
+
+    arrete_nez1 = cv2.circle(img, (int(round(x)), y + 10), 1, (255,0,0), 2)
+    arrete_nez2 = cv2.circle(img, (int(round(x)), y + 20), 1, (255,0,0), 2)
+    arrete_nez3 = cv2.circle(img, (int(round(x)), y + 30), 1, (255,0,0), 2)
+    arrete_nez4 = cv2.circle(img, (int(round(x)), y + 40), 1, (255,0,0), 2)
+    arrete_nez5 = cv2.circle(img, (int(round(x)), y + 50), 1, (255,0,0), 2)
+    arrete_nez6 = cv2.circle(img, (int(round(x)), y + 60), 1, (255,0,0), 2)
+
+    ange1 = cv2.circle(img, (int(round(x)), y + 90), 1, (255,0,0), 2)
+    ange2 = cv2.circle(img, (int(round(x)), y + 100), 1, (255,0,0), 2)
+    ange3 = cv2.circle(img, (int(round(x)), y + 110), 1, (255,0,0), 2)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
