43fa87e208ce4d02923d7a11d274f424fb1998c2
LeGrosLezard <53193299+LeGrosLezard@users.noreply.github.com>
2019-08-11 00:21:40 +0200

    Update main.py

--- yeux/clignement/main.py
+++ yeux/clignement/main.py
@@ -3,54 +3,127 @@ import cv2
 from PIL import Image
 import os
 
-from traitement_image import *
+from traitement import *
+
+
+
 
 
 def video_capture():
 
-    LISTE_AJUSTEMENT = []
-    LISTE_DROITE_GAUCHE = []
-    LISTE_QUALIBRAGE = []
+    LISTE = []
+
+    Ltempe_gauche = []
+    Ltempe_droite = []
+    
+    Loreille_gauche1 = []
+    Loreille_gauche2 = []
+    Loreille_gauche3 = []
+    Loreille_gauche4 = []
+    Loreille_gauche5 = []
+    Loreille_gauche6 = []
+    
+    Loreille_droite1 = []
+    Loreille_droite2 = []
+    Loreille_droite3 = []
+    Loreille_droite4 = []
+    Loreille_droite5 = []
+    Loreille_droite6 = []
+
+    Lcerne_droite1 = []
+    Lcerne_droite2 = []
+    Lcerne_droite3 = []
+    Lcerne_droite4 = []
+    Lcerne_droite5 = [] 
+
+    Lcerne_gauche1 = []
+    Lcerne_gauche2 = []
+    Lcerne_gauche3 = []
+    Lcerne_gauche4 = []
+    Lcerne_gauche5 = []
+
+    Lpomette_droite = []
+    Lpomette_gauche = []
+
+    Lfront1 = []
+    Lfront2 = []
+    Lfront3 = []
+    Lfront4 = []
+
+    Lbouche1 = []
+    Lbouche2 = []
+    Lbouche3 = []
+    Lbouche4 = []
+    Lbouche5 = []
+
+
+    Lmillieu = []
+
+    Lsourcile61 = []
+    Lsourcile62 = []
+    Lsourcile63 = []
+    Lsourcile7 = []
+    Lsourcile8 = []
+    Lsourcile9 = []
+    Lsourcile10 = []
+
+
+    Lsourcile1 = []
+    Lsourcile2 = []
+    Lsourcile3 = []
+    Lsourcile4 = []
+    Lsourcile5 = []
+
+
 
     video = cv2.VideoCapture(0)
+    faceCascade = cv2.CascadeClassifier("haarcascade_frontalface_alt2.xml")
+    eyesCascade = cv2.CascadeClassifier('haarcascade_eye.xml')
 
-    #LE RETOUR DES YEUX IMPORT si retour alors déplcement des yeux same for gueule
+    
     while(True):
 
 
-        left_eye = cv2.CascadeClassifier('haarcascade_lefteye_2splits.xml')
-        
-          
         ret, frame = video.read()
         frame = cv2.resize(frame, (600, 600))
-        eyes = left_eye.detectMultiScale(frame)
 
-        
-        if len(LISTE_AJUSTEMENT) < 50:
-            pre_initialisation(eyes, LISTE_AJUSTEMENT, frame)
-            print("initialisation")
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+        if len(Lsourcile1) < 50:
+            initialisation(frame, video, faceCascade, gray,
+                           Ltempe_gauche, Ltempe_droite,
+                           Loreille_gauche1, Loreille_gauche2,
+                           Loreille_gauche3, Loreille_gauche4,
+                           Loreille_gauche5, Loreille_gauche6,
+                           Loreille_droite1, Loreille_droite2,
+                           Loreille_droite3, Loreille_droite4,
+                           Loreille_droite5, Loreille_droite6,
+                           Lcerne_droite1, Lcerne_droite2,
+                           Lcerne_droite3, Lcerne_droite4,
+                           Lcerne_droite5, Lcerne_gauche1,
+                           Lcerne_gauche2, Lcerne_gauche3,
+                           Lcerne_gauche4, Lcerne_gauche5,
+                           Lpomette_droite, Lpomette_gauche,
+                           Lfront1, Lfront2, Lfront3, Lfront4,
+                           Lbouche1, Lbouche2, Lbouche3, Lbouche4,
+                           Lbouche5, Lmillieu, Lsourcile61, Lsourcile62,
+                           Lsourcile63, Lsourcile7,
+                           Lsourcile8, Lsourcile9, Lsourcile10, Lsourcile1,
+                           Lsourcile2, Lsourcile3, Lsourcile4, Lsourcile5,
+                           eyesCascade)
 
         else:
-            position1 = position_yeux_verticale(eyes, LISTE_AJUSTEMENT, frame)
-            position2 = position_yeux_horizontal(eyes, LISTE_DROITE_GAUCHE, frame)
 
-            association(position1, position2, LISTE_AJUSTEMENT)
+            yeux(frame, video, eyesCascade,
+                 Lsourcile61, Lsourcile62,
+                 Lsourcile63,
+                 Lsourcile7, Lsourcile8,
+                 Lsourcile9, Lsourcile10)
+            
+            figure(frame, video, faceCascade, gray)
+            
+            cv2.imshow('FACE CAPTURE', frame)
 
-            #---------------------------------------------------Partie effacage de liste
-            #permet d'effacer la liste et de refaire une initialisation
-            LISTE_QUALIBRAGE.append(position1)
-            qualibration = qualibrage(LISTE_QUALIBRAGE)
-            if qualibration == "qualibration":
-                LISTE_AJUSTEMENT = []
-
-            if position1 in ("le mec s'est baissé", "le mec s'est levé",
-                             "le mec à levé la tete", "le mec à baisser la tete"):
-                LISTE_AJUSTEMENT = []
-
-
-
-
-        cv2.imshow('YEUX CAPTURE', frame)
 
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
