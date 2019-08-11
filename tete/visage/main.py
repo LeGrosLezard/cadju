d7d8ec36620fb73a57eda3c6ce3b1a392538bef0
LeGrosLezard <53193299+LeGrosLezard@users.noreply.github.com>
2019-08-11 19:25:12 +0200

    Update main.py
    bon le but c de dire si l'endroit noir deviens blanc c qu'un truk blanc qu'il vient de se gratter
    si un petit espace alors c qu'il se gratte la tempe par contrefaut pas que le mec soit chauve

--- tete/visage/main.py
+++ tete/visage/main.py
@@ -3,25 +3,13 @@ import cv2
 from PIL import Image
 import os
 
-from traitement import *
-from initialisation import *
 
 
+from traitement import decoupage
 
 
 def video_capture():
 
-
-    CONFIG_OEIL_DROIT = [0]
-    CONFIG_INITIALISATION = []
-
-    Lsourcile61 = []
-    Lsourcile62 = []
-    Lsourcile63 = []
-
-    Lsourcile71 = []
-    Lsourcile72 = []
-    Lsourcile73 = []
 
 
     video = cv2.VideoCapture(0)
@@ -35,28 +23,16 @@ def video_capture():
         ret, frame = video.read()
         frame = cv2.resize(frame, (600, 600))
 
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        decoupage(frame, faceCascade)
 
-        if len(CONFIG_INITIALISATION) < 50:
-            initialisation(frame, video, faceCascade, eyesCascade, gray,
-                   CONFIG_INITIALISATION)
 
-            
-            
-        else:
 
-            yeux(frame, video, eyesCascade, faceCascade, gray,
-                 Lsourcile61, Lsourcile62, Lsourcile63,
-                 Lsourcile71, Lsourcile72, Lsourcile73,
-                 CONFIG_OEIL_DROIT, CONFIG_INITIALISATION)
-            
-            
-        #figure(frame, video, faceCascade, gray)
-            
+        
         cv2.imshow('FACE INITIALISATION', frame)
 
-
-
+        if cv2.waitKey(1) & 0xFF == ord('a'):
+            cv2.imwrite("jb.jpg", frame)
+        
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
