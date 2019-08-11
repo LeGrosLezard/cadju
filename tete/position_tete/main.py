b98d305fde417e2ce38843c1eab5b07b286da0ad
LeGrosLezard <53193299+LeGrosLezard@users.noreply.github.com>
2019-08-11 14:20:46 +0200

    Update main.py

--- tete/position_tete/main.py
+++ tete/position_tete/main.py
@@ -10,29 +10,31 @@ def video_capture():
 
     LISTE = []
 
-    
     video = cv2.VideoCapture(0)
     faceCascade = cv2.CascadeClassifier("haarcascade_frontalface_alt2.xml")
-    eyes = cv2.CascadeClassifier('haarcascade_lefteye_2splits.xml')
+    eyesCascade = cv2.CascadeClassifier('haarcascade_eye.xml')
+
+
     
     while(True):
 
 
         ret, frame = video.read()
         frame = cv2.resize(frame, (600, 600))
+ 
         if len(LISTE) < 50:
-            initialisation(video, frame, faceCascade, eyes, LISTE)
+            initialisation(video, frame, faceCascade, eyesCascade, LISTE)
             
         else:
-            position1 = position_tete(video, frame, faceCascade, eyes, LISTE)
+            position1 = position_tete(video, frame, faceCascade, eyesCascade, LISTE)
 
         
-
-
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
+   
 
+        
     video.release()
     cv2.destroyAllWindows()
 
