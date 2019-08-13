e3c598877a5f60eafab98eef20418a903857d65b
LeGrosLezard <53193299+LeGrosLezard@users.noreply.github.com>
2019-08-13 23:48:47 +0200

    Update tete.py
    bon demain fini les truks faut absolument finir et le cv et ensuite tu pourras faire oreille et le reste en gros ca va etre une video static MAIS avec full initia

--- tete/visage/tete.py
+++ tete/visage/tete.py
@@ -2,70 +2,13 @@ import numpy as np
 import cv2
 
 
-
-
-
-def detect_box(x, y, y1,y2, w,
-               BOX, frame):
-
-    liste1 = []
-
-    for i in range(y1, y2):
-        for j in range(x, x+w):
-            liste1.append(frame[i,j][0])
-
-    liste1 = str(liste1)
-
-    
-    try:
-        if sum(BOX)/len(BOX) + 1400 < len(liste1):
-            cv2.rectangle(frame, (x, y1),
-                          (x+w, y2), (0,0,255), 1)
-            print("MAIN AU DESSUS DE LA TETE")
-            
-        elif sum(BOX)/len(BOX) - 1400 > len(liste1):
-            cv2.rectangle(frame, (x, y1),
-                          (x+w, y2), (255,0,255), 1)
-            print("MAIN AU DESSUS DE LA TETE")
-            
-        else:
-            cv2.rectangle(frame, (x, y1),
-                          (x+w, y2), 1)
-            BOX.append(len(liste1))
-
-    except:
-        cv2.rectangle(frame, (x, y1),
-                      (x+w, y2), 1)
-        BOX.append(len(liste1))
-            
-
-def tete_haut(faceCascade, frame, x, y, h, w,
-              BOX_ONE):
-    
-    x = x.tolist()
-    y = y.tolist()
-    h = h.tolist()
-    w = w.tolist()
-
-    print(x,y,h,w)
-
-    dessus_visage1 = int(round(60 * 100 / h))
-    dessus_visage2 = int(round(150 * 100 / h))
-
-
-    detect_box(x, y, y-dessus_visage2,
-               y-dessus_visage1, w, BOX_ONE, frame)
-
-    
-
-
 def position_tete(x, w, POSITION_TETE):
 
-    if x + w > sum(POSITION_TETE) / len(POSITION_TETE) + 20:
+    if x + w > sum(POSITION_TETE) / len(POSITION_TETE) + 40:
         print("le mec s'est rapproché")
         return "reposition"
 
-    elif x + w < sum(POSITION_TETE) / len(POSITION_TETE) - 20:
+    elif x + w < sum(POSITION_TETE) / len(POSITION_TETE) - 40:
         print("le mec s'est éloigné")
         return "reposition"
 
@@ -73,8 +16,13 @@ def position_tete(x, w, POSITION_TETE):
         POSITION_TETE.append(x+w)
 
 
+
+
 def detection_face(faceCascade, frame, BOX_ONE, POSITION_TETE):
+
+
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
 
     faces = faceCascade.detectMultiScale(
         gray,
@@ -84,13 +32,20 @@ def detection_face(faceCascade, frame, BOX_ONE, POSITION_TETE):
         flags=cv2.CASCADE_SCALE_IMAGE
     )
 
-    
+
 
     for x, y, w, h in faces:
-        cv2.rectangle(frame, (x,y), (x+w, y+h),(0, 0, 255), 2)
         
-        tete_haut(faceCascade, frame, x, y, h, w, BOX_ONE)
+        cv2.rectangle(frame, (x,y), (x+w, y+h), (0, 0, 255), 2)
+        
         reposition = position_tete(x, w, POSITION_TETE)
+
+        liste1 = []
+        x = x.tolist()
+        y = y.tolist()
+        h = h.tolist()
+        w = w.tolist()
+
 
         if reposition == "reposition":
             out = "reposition"
@@ -98,6 +53,40 @@ def detection_face(faceCascade, frame, BOX_ONE, POSITION_TETE):
             out = None
 
         return out
+
+
+def haut_tete(frame, a, b, c, d, subtractor):
+    
+    cv2.rectangle(frame, (a,c), (b, d),(0, 0, 255), 2)
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    crop = gray[d:c, a:b]
+
+    mask = subtractor.apply(crop)
+
+    liste1 = []
+    for i in mask:
+        for j in i:
+            liste1.append(j)
+
+
+    print(sum(liste1) / len(liste1))
+
+
+  
+    return mask
+  
+        
+
+
+
+
+
+
+
+
+
 
 
 
