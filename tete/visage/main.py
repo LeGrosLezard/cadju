55e492966cab837bb0557a8d958786384e203e10
LeGrosLezard <53193299+LeGrosLezard@users.noreply.github.com>
2019-08-13 23:47:52 +0200

    Update main.py
    fallait pas coller le mask a la tete... faut re ini si le mec se balance de gauche adroite

--- tete/visage/main.py
+++ tete/visage/main.py
@@ -6,6 +6,7 @@ import os
 
 
 from tete import detection_face
+from tete import haut_tete
 from initialisation import initialisation
 
 
@@ -14,13 +15,20 @@ def video_capture():
 
     BOX_ONE = []
     POSITION_TETE = []
+    
+    POSITION_HAUT_TETE_X = []
+    POSITION_HAUT_TETE_Y1 = []
+    POSITION_HAUT_TETE_W = []
+    POSITION_HAUT_TETE_Y2 = []
 
 
     video = cv2.VideoCapture(0)
     faceCascade = cv2.CascadeClassifier("haarcascade_frontalface_alt2.xml")
     eyesCascade = cv2.CascadeClassifier('haarcascade_eye.xml')
-
     
+    subtractor = cv2.createBackgroundSubtractorMOG2(history=100,
+                                                    varThreshold=50,
+                                                    detectShadows=True)
     while(True):
 
 
@@ -28,24 +36,42 @@ def video_capture():
         frame = cv2.resize(frame, (600, 600))
 
 
-        if len(POSITION_TETE) < 20:
-            initialisation(frame, faceCascade, POSITION_TETE)
+        if len(POSITION_TETE) < 5:
+            initialisation(frame, faceCascade, POSITION_TETE,
+                            POSITION_HAUT_TETE_X,
+                            POSITION_HAUT_TETE_Y1,
+                            POSITION_HAUT_TETE_W,
+                            POSITION_HAUT_TETE_Y2)
             print("initialisation")
         else:
-            reposition = detection_face(faceCascade, frame,
-                                         BOX_ONE, POSITION_TETE)
 
+            a = int(round(sum(POSITION_HAUT_TETE_X)/len(POSITION_HAUT_TETE_X)))
+            b = int(round(sum(POSITION_HAUT_TETE_W)/len(POSITION_HAUT_TETE_W)))
+            c = int(round(sum(POSITION_HAUT_TETE_Y1)/len(POSITION_HAUT_TETE_Y1)))
+            d = int(round(sum(POSITION_HAUT_TETE_Y2)/len(POSITION_HAUT_TETE_Y2)))
+
+            
+            mask = haut_tete(frame, a, b, c, d, subtractor)
+            
+            reposition = detection_face(faceCascade, frame, BOX_ONE,
+                                        POSITION_TETE)
+
+        
             if reposition == "reposition":
                 POSITION_TETE = []
+                
             else:
                 pass
 
-        
+       
+
+            
         cv2.imshow('FACE', frame)
-
-
-
-
+        try:
+            cv2.imshow('MASK', mask)
+        except:
+            pass
+        
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
