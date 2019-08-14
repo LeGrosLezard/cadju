e7ed29401a4381a7aba392e391af0cd37eb94eab
LeGrosLezard <53193299+LeGrosLezard@users.noreply.github.com>
2019-08-14 02:07:11 +0200

    Update initialisation.py

--- tete/visage/initialisation.py
+++ tete/visage/initialisation.py
@@ -17,6 +17,7 @@ def detection_face(frame, faceCascade):
     for x, y, w, h in faces:
         cv2.rectangle(frame, (x,y), (x+w, y+h),(0, 0, 0), 2)
 
+
     try:
         x = x.tolist()
         w = w.tolist()
@@ -30,10 +31,23 @@ def detection_face(frame, faceCascade):
 
 
 def position_haut_tete(faceCascade, frame,
-                       POSITION_HAUT_TETE_X,
-                       POSITION_HAUT_TETE_Y1,
-                       POSITION_HAUT_TETE_W,
-                       POSITION_HAUT_TETE_Y2):
+                        POSITION_HAUT_TETE_X,
+                        POSITION_HAUT_TETE_Y1,
+                        POSITION_HAUT_TETE_W,
+                        POSITION_HAUT_TETE_Y2,
+                        POSITION_HAUT_TETE_X_1,
+                        POSITION_HAUT_TETE_Y1_1,
+                        POSITION_HAUT_TETE_W_1,
+                        POSITION_HAUT_TETE_Y2_1,
+                        POSITION_HAUT_TETE_X_2,
+                        POSITION_HAUT_TETE_Y1_2, 
+                        POSITION_HAUT_TETE_W_2, 
+                        POSITION_HAUT_TETE_Y2_2,
+                        POSITION_HAUT_TETE_X_3,
+                        POSITION_HAUT_TETE_Y1_3, 
+                        POSITION_HAUT_TETE_W_3, 
+                        POSITION_HAUT_TETE_Y2_3):
+
     
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
@@ -62,33 +76,74 @@ def position_haut_tete(faceCascade, frame,
         dessus_visage1 = int(round(60 * 100 / h))
         dessus_visage2 = int(round(150 * 100 / h))
 
-        cv2.rectangle(frame, (x, y-dessus_visage1),
-                      (x+w, y-dessus_visage2),(0, 0, 255), 2)
+        carre = int(round(w/3))
 
-        POSITION_HAUT_TETE_X.append(x)
-        POSITION_HAUT_TETE_W.append(x+w)
-        POSITION_HAUT_TETE_Y1.append(y-dessus_visage1)
-        POSITION_HAUT_TETE_Y2.append(y-dessus_visage2)
-
+        cv2.rectangle(frame,(x-20 ,y-dessus_visage1), (x + carre-20, y - dessus_visage2),
+                      (0,255,0), 1)
         
+        cv2.rectangle(frame,(x + carre, y-dessus_visage1),
+                      (x + carre*2, y - dessus_visage2), (255,0,0), 1)
+        
+        cv2.rectangle(frame,(x+ carre*2 + 15, y-dessus_visage1),
+                      ((x + carre*3) + 15, y - dessus_visage2), (0,0,255), 1)
+
+
+        POSITION_HAUT_TETE_X_1.append(x-20)
+        POSITION_HAUT_TETE_Y1_1.append(y-dessus_visage1)
+        POSITION_HAUT_TETE_W_1.append(x + carre-20)
+        POSITION_HAUT_TETE_Y2_1.append(y-dessus_visage2)
+        
+        POSITION_HAUT_TETE_X_2.append(x + carre)
+        POSITION_HAUT_TETE_Y1_2.append(y-dessus_visage1)
+        POSITION_HAUT_TETE_W_2.append(x + carre*2)
+        POSITION_HAUT_TETE_Y2_2.append(y-dessus_visage2)
+        
+        POSITION_HAUT_TETE_X_3.append(x + carre*2 + 15)
+        POSITION_HAUT_TETE_Y1_3.append(y-dessus_visage1)
+        POSITION_HAUT_TETE_W_3.append(x + carre*3 + 15)
+        POSITION_HAUT_TETE_Y2_3.append(y-dessus_visage2)      
 
         
 
 def initialisation(frame, faceCascade, POSITION_TETE,
-                   POSITION_HAUT_TETE_X,
+                    POSITION_HAUT_TETE_X,
                     POSITION_HAUT_TETE_Y1,
                     POSITION_HAUT_TETE_W,
-                    POSITION_HAUT_TETE_Y2):
+                    POSITION_HAUT_TETE_Y2,
+                    POSITION_HAUT_TETE_X_1,
+                    POSITION_HAUT_TETE_Y1_1,
+                    POSITION_HAUT_TETE_W_1,
+                    POSITION_HAUT_TETE_Y2_1,
+                    POSITION_HAUT_TETE_X_2,
+                    POSITION_HAUT_TETE_Y1_2, 
+                    POSITION_HAUT_TETE_W_2, 
+                    POSITION_HAUT_TETE_Y2_2,
+                    POSITION_HAUT_TETE_X_3,
+                    POSITION_HAUT_TETE_Y1_3, 
+                    POSITION_HAUT_TETE_W_3, 
+                    POSITION_HAUT_TETE_Y2_3):
 
     try:
         x, w = detection_face(frame, faceCascade)
 
         POSITION_TETE.append(x + w)
         position_haut_tete(faceCascade, frame,
-                       POSITION_HAUT_TETE_X,
-                       POSITION_HAUT_TETE_Y1,
-                       POSITION_HAUT_TETE_W,
-                       POSITION_HAUT_TETE_Y2)
+                            POSITION_HAUT_TETE_X,
+                            POSITION_HAUT_TETE_Y1,
+                            POSITION_HAUT_TETE_W,
+                            POSITION_HAUT_TETE_Y2,
+                            POSITION_HAUT_TETE_X_1,
+                            POSITION_HAUT_TETE_Y1_1,
+                            POSITION_HAUT_TETE_W_1,
+                            POSITION_HAUT_TETE_Y2_1,
+                            POSITION_HAUT_TETE_X_2,
+                            POSITION_HAUT_TETE_Y1_2, 
+                            POSITION_HAUT_TETE_W_2, 
+                            POSITION_HAUT_TETE_Y2_2,
+                            POSITION_HAUT_TETE_X_3,
+                            POSITION_HAUT_TETE_Y1_3, 
+                            POSITION_HAUT_TETE_W_3, 
+                            POSITION_HAUT_TETE_Y2_3)
         
     except:
         pass
