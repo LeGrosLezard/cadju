8bc954b5c78b0f184dc6bdee20153313b70fe315
LeGrosLezard <53193299+LeGrosLezard@users.noreply.github.com>
2019-08-15 00:51:28 +0200

    Update main.py

--- tete/visage/main.py
+++ tete/visage/main.py
@@ -10,6 +10,10 @@ from initialisation import init_cotes_frame
 from initialisation import init_bouche
 from initialisation import init_menton
 from initialisation import init_buste
+from initialisation import init_epaul
+from initialisation import init_front
+from initialisation import init_tempes
+from initialisation import init_oreille
 
 
 from tete import milieu_haut_tete
@@ -18,6 +22,28 @@ from tete import cotes_frame
 from tete import bouche
 from tete import menton
 from tete import buste
+from tete import epaul
+from tete import front
+from tete import tempe
+from tete import oreille
+
+
+from config import SUBSTRACTOR
+from config import SUBSTRACTOR1
+from config import SUBSTRACTOR2
+from config import SUBSTRACTOR3
+from config import SUBSTRACTOR4
+from config import SUBSTRACTOR5
+from config import SUBSTRACTOR6
+from config import SUBSTRACTOR7
+from config import SUBSTRACTOR8
+from config import SUBSTRACTOR9
+from config import SUBSTRACTOR10
+from config import SUBSTRACTOR11
+from config import SUBSTRACTOR12
+from config import SUBSTRACTOR13
+from config import SUBSTRACTOR14
+
 
 
 
@@ -30,13 +56,16 @@ def video_capture():
     BOUCHE = [[], [], [], []]
     MENTON = [[], [], [], []]
     BUSTE = [[], [], [], []]
+    EPAUL = [[], [], [], [], [], [], [], []]
+    FRONT = [[], [], [], []]
+    TEMPE = [[], [], [], [], [], [], [], []]
+    OREILLE = [[], [], [], [], [], [], [], []]
     
     video = cv2.VideoCapture(0)
     faceCascade = cv2.CascadeClassifier("haarcascade_frontalface_alt2.xml")
+
     
-    subtractor = cv2.createBackgroundSubtractorMOG2(history=100,
-                                                    varThreshold=50,
-                                                    detectShadows=True)
+
 
     while(True):
 
@@ -53,18 +82,26 @@ def video_capture():
             init_bouche(frame, faceCascade, BOUCHE)
             init_menton(frame, faceCascade, MENTON)
             init_buste(frame, faceCascade, BUSTE)
+            init_epaul(frame, faceCascade, EPAUL)
+            init_front(frame, faceCascade, FRONT)
+            init_tempes(frame, faceCascade, TEMPE)
+            init_oreille(frame, faceCascade, OREILLE)
+
+
             
         else:
-            milieu_haut_tete(frame, MILIEU_TETE)
-            cotes_tete(frame, COTE_TETE)
-            cotes_frame(frame, COTE_FRAME)
-            bouche(frame, BOUCHE)
-            menton(frame, MENTON)
-            buste(frame, BUSTE)
+            milieu_haut_tete(frame, MILIEU_TETE, SUBSTRACTOR)
+            cotes_tete(frame, COTE_TETE, SUBSTRACTOR1, SUBSTRACTOR2)
+            cotes_frame(frame, COTE_FRAME, SUBSTRACTOR3, SUBSTRACTOR4)
+            bouche(frame, BOUCHE, SUBSTRACTOR5)
+            menton(frame, MENTON, SUBSTRACTOR6)
+            buste(frame, BUSTE, SUBSTRACTOR7)
+            epaul(frame, EPAUL, SUBSTRACTOR8, SUBSTRACTOR9)
+            front(frame, FRONT, SUBSTRACTOR10)
+            tempe(frame, TEMPE, SUBSTRACTOR11, SUBSTRACTOR12)
+            oreille(frame, OREILLE, SUBSTRACTOR13, SUBSTRACTOR14)
 
-
-        
-       
+    
         cv2.imshow('FACE', frame)
 
         
