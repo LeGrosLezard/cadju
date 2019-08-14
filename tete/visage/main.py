e1e8c585ee02c3f8aed99d1314a28e30048927bd
LeGrosLezard <53193299+LeGrosLezard@users.noreply.github.com>
2019-08-14 19:46:28 +0200

    Update main.py
    va falloir faire full initialisage enfete, mais faut le faire len(1) sinon la ca va bug
    
    faut faire sourcile, nez, fronz, nuk, épaul, joue, oreille, tempe

--- tete/visage/main.py
+++ tete/visage/main.py
@@ -4,12 +4,20 @@ from PIL import Image
 import os
 
 
+from initialisation import init_tete_haut
+from initialisation import init_cotes_tete
+from initialisation import init_cotes_frame
+from initialisation import init_bouche
+from initialisation import init_menton
+from initialisation import init_buste
+
 
 from tete import milieu_haut_tete
-
-
-from initialisation import init_tete_haut
-
+from tete import cotes_tete
+from tete import cotes_frame
+from tete import bouche
+from tete import menton
+from tete import buste
 
 
 
@@ -18,7 +26,11 @@ def video_capture():
 
     MILIEU_TETE = [[], [], [], []]
     COTE_TETE = [[], [], [], [], [], [], [], []]
-
+    COTE_FRAME = [[], [], [], [], [], [], [], []]
+    BOUCHE = [[], [], [], []]
+    MENTON = [[], [], [], []]
+    BUSTE = [[], [], [], []]
+    
     video = cv2.VideoCapture(0)
     faceCascade = cv2.CascadeClassifier("haarcascade_frontalface_alt2.xml")
     
@@ -34,11 +46,21 @@ def video_capture():
 
 
 
-        if len(MILIEU_TETE[0]) < 10:
+        if len(MILIEU_TETE[0]) < 1:
             init_tete_haut(frame, faceCascade, MILIEU_TETE)
+            init_cotes_tete(frame, faceCascade, COTE_TETE)
+            init_cotes_frame(frame, faceCascade, COTE_FRAME)
+            init_bouche(frame, faceCascade, BOUCHE)
+            init_menton(frame, faceCascade, MENTON)
+            init_buste(frame, faceCascade, BUSTE)
             
         else:
             milieu_haut_tete(frame, MILIEU_TETE)
+            cotes_tete(frame, COTE_TETE)
+            cotes_frame(frame, COTE_FRAME)
+            bouche(frame, BOUCHE)
+            menton(frame, MENTON)
+            buste(frame, BUSTE)
 
 
         
