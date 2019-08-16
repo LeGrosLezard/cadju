e740ff9f7137b97c3c5270ada4fa96f873bf03a6
LeGrosLezard <53193299+LeGrosLezard@users.noreply.github.com>
2019-08-16 23:04:57 +0200

    Update main.py
    mtn faut le mettre avec mon code sans mon super detecteur de peau ca marchait moins bien u_u pfff ca marche que dal essais de rendre ca bien

--- mains/main.py
+++ mains/main.py
@@ -11,7 +11,7 @@ video = cv2.VideoCapture(0)
 faceCascade = cv2.CascadeClassifier("haarcascade_frontalface_alt2.xml")
 eyesCascade = cv2.CascadeClassifier('haarcascade_eye.xml')
 
-subtractor = cv2.createBackgroundSubtractorMOG2(history=100,
+subtractor = cv2.createBackgroundSubtractorMOG2(history=10000,
                                                 varThreshold=50,
                                                 detectShadows=True)
 
@@ -25,28 +25,47 @@ liste3 = []
 while(True):
 
     
-    
     _, frame = video.read()
+    frame = cv2.GaussianBlur(frame,(5,5),100)
+    
 
-    if len(liste1) < 1:
+    if len(liste1) < 50:
         crop_face(frame, faceCascade, eyesCascade, BOX1,
                   liste1, liste2, liste3)
-        cv2.imshow("mask", frame)
-        
+
     else:
+        try:
+            delete_face(frame, faceCascade)
+        except:
+            pass
+        
         image_mask = detect_skin(liste1, liste2, liste3, frame)
-        cv2.imshow('FACE', image_mask)
-    
-
-##    contours =  cv2.findContours(frame,
-##                                 cv2.RETR_TREE,
-##                                 cv2.CHAIN_APPROX_SIMPLE)[-2]
-##    
-##    for i in contours:
-##        cv2.drawContours(frame, i, -1, (0, 0, 255), 3)
+        
+        blur = cv2.GaussianBlur(image_mask,(5,5),0)
+        ret,thresh1 = cv2.threshold(blur,100,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+        contours, hierarchy = cv2.findContours(thresh1,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
 
 
-    
+        max_area = 0
+        for i in range(len(contours)):
+            cnt=contours[i]
+            area = cv2.contourArea(cnt)
+            if(area>max_area):
+                max_area=area
+                ci=i
+        hull = cv2.convexHull(cnt)
+
+        
+        cnt=contours[ci]
+        drawing = np.zeros(frame.shape,np.uint8)
+        cv2.drawContours(drawing,[cnt],0,(0,255,0),2)
+        cv2.drawContours(drawing,[hull],0,(0,0,255),2)
+
+
+
+        cv2.imshow('frame', image_mask)
+        cv2.imshow('frame', thresh1)
+        cv2.imshow("image.jpg", drawing)
 
 
     
