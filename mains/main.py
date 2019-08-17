0b6b089bdbd5d5744cd8bde5f486fd4f0a465a88
LeGrosLezard <53193299+LeGrosLezard@users.noreply.github.com>
2019-08-17 22:39:53 +0200

    Add files via upload
    ok pause,
    
    c CA QUE JVOULAIS DEPUIS LE DEBUT
    j'ai plein de detecteur de main une pour le volan qui m'aurait servi de haar pour les pouce, et les autres qui marche assez ok mais voila oufffff

---
+++ mains/main.py
@@ -0,0 +1,61 @@
+import cv2
+import time
+import datetime
+import imutils
+
+
+def motion_detection():
+    video_capture = cv2.VideoCapture(0) 
+    time.sleep(2)
+
+    first_frame = None
+
+    while True:
+
+        
+        _, frame = video_capture.read()
+        
+        greyscale_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        gaussian_frame = cv2.GaussianBlur(greyscale_frame, (21,21),0)
+        blur_frame = cv2.blur(gaussian_frame, (5,5)) 
+
+        greyscale_image = blur_frame
+
+        
+        if first_frame is None:
+            first_frame = greyscale_image 
+
+
+        frame = imutils.resize(frame, width=500)
+        
+        frame_delta = cv2.absdiff(first_frame, greyscale_image) 
+        thresh = cv2.threshold(frame_delta, 100, 255, cv2.THRESH_BINARY)[1]
+
+        dilate_image = cv2.dilate(thresh, None, iterations=2)
+
+
+        cnt,_ = cv2.findContours(dilate_image.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[-2:]
+
+        for c in cnt:
+            if cv2.contourArea(c) > 800: 
+                (x, y, w, h) = cv2.boundingRect(c)
+
+                cv2.rectangle(frame, (x,y), (x+w, y+h), (0, 255, 0), 2)
+
+
+        cv2.imshow('Security Feed', frame)
+        cv2.imshow('Threshold(foreground mask)', dilate_image)
+        cv2.imshow('Frame_delta', frame_delta)
+
+
+
+
+        key = cv2.waitKey(1) & 0xFF
+        if key == ord('q'):
+            cv2.destroyAllWindows()
+            break
+                
+
+
+if __name__=='__main__':    
+    motion_detection()
