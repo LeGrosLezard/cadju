618808de815ed4c75e7c426faa87eccb8cd168c7
LeGrosLezard <53193299+LeGrosLezard@users.noreply.github.com>
2019-08-19 22:39:17 +0200

    Update traitement_hand.py

--- mains/traitement_hand.py
+++ mains/traitement_hand.py
@@ -63,7 +63,7 @@ def contour_image(dilate_image):
     imgray = cv2.cvtColor(image_detector, cv2.COLOR_BGR2GRAY)
     ret, thresh = cv2.threshold(imgray, 127, 255, 0)
     contours, hierarchy = cv2.findContours(thresh,
-                                           cv2.RETR_EXTERNAL,
+                                           cv2.RETR_CCOMP,
                                            cv2.CHAIN_APPROX_SIMPLE)
 
 
@@ -80,14 +80,13 @@ def hull_function(contours, thresh, hierarchy):
     
     hull = []
      
-
     for i in range(len(contours)):
         hull.append(cv2.convexHull(contours[i], False))
 
     drawing = np.zeros((thresh.shape[0], thresh.shape[1], 3), np.uint8)
 
     for i in range(len(contours)):
-        if len(contours[i]) < 150:
+        if len(contours[i]) < 200:
             pass
         else:
             color_contours = (0, 255, 0) 
@@ -95,6 +94,61 @@ def hull_function(contours, thresh, hierarchy):
     
             cv2.drawContours(drawing, contours, i, color_contours, 1, 8, hierarchy)
             cv2.drawContours(drawing, hull, i, color, 1, 8)
+            
+    return drawing, hull
+
+
+
+def points_main(drawing, hull):
+
+    image_detector = cv2.imread("image_detector.jpg")
+    pts_x = [[], [], []]
+    pts_y = [[], [], []]
+    dico = {}
+    c = 0
+    for i in hull:
+        i = i.tolist()
+        for j in i:
+            dico[j[0][0]] = j[0][1]
+            pts_x[0].append(j[0][0])
+            pts_y[0].append(j[0][1])
+
+    return pts_x, pts_y
+
+
+
+def points(pts_x, pts_y, drawing):
+
+    image_detector = cv2.imread("image_detector.jpg")
+
+    l = h = image_detector.shape[1]
+    h = image_detector.shape[0]
+    h1 = int(round(h * 20 / 100))
+
+    for i in range(2):
+        for i in range(len(pts_x[0])):
+            try:
+                if pts_x[0][i] >= pts_x[0][i + 1] and\
+                   pts_x[0][i] <= pts_x[0][i + 1] + 4 or\
+                   pts_x[0][i] + 3 >= pts_x[0][i + 1] and\
+                   pts_x[0][i] <= pts_x[0][i + 1] + 1 or\
+                   pts_y[0][i] >= pts_y[0][i + 1] and\
+                   pts_y[0][i] <= pts_y[0][i + 1] + 8:
+                    pass
+                elif pts_y[0][i] > h-h1:
+                    pass
+                else:
+                    pts_x[1].append(pts_x[0][i])
+                    pts_y[1].append(pts_y[0][i])
+            except:
+                pass
+
+
+    for i in range(len(pts_x[1])):
+        cv2.circle(drawing, (pts_x[1][i], pts_y[1][i]), 3, (0,0,255), 3)
+        
+
+
 
 
     return drawing
