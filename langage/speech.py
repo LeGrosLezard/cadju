7afaef054b9b5d63300b47ae9766fd9abc3d0c7e
LeGrosLezard <53193299+LeGrosLezard@users.noreply.github.com>
2019-08-21 00:30:35 +0200

    Update speech.py
    fait chié portaudio jtai trop content

--- langage/speech.py
+++ langage/speech.py
@@ -1,1 +1,13 @@
 import speech_recognition as sr
+
+r=sr.Recognizer()
+
+with sr.AudioFile("dictée.wav") as source:
+    audio = r.record(source)
+    audio = r.listen(source)
+
+    
+text = r.recognize_google(audio)
+for i in text:
+    print(i)
+
