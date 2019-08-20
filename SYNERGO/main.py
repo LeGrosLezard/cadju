a31b1301576a3232f7fcf88fd8d16734c4d911b2
LeGrosLezard <53193299+LeGrosLezard@users.noreply.github.com>
2019-08-20 02:24:26 +0200

    Update main.py
    bon j'en ai marre faut faire une fonction pour la tete, et faire les affichages depuis main, et trouver coment réduire et voir se qui coute le plus de ram les boucles ? les appel de fonction ? chai pas

--- SYNERGO/main.py
+++ SYNERGO/main.py
@@ -1,46 +1,68 @@
 import cv2
-import time
-import datetime
-import imutils
-import numpy as np
-
-
 
 from mains.traitement_hand import delete_visage
 from mains.traitement_hand import first_window
 from mains.traitement_hand import seconde_window
 from mains.traitement_hand import contour_image
 from mains.traitement_hand import hull_function
-from mains.traitement_hand import time
 from mains.traitement_hand import points_main
 from mains.traitement_hand import points
 from mains.traitement_hand import hand_function
 
 
+from head_position.head_inclinaison import initialisation
+from head_position.head_inclinaison import position_oeil
+from head_position.head_inclinaison import position_tete
+
+
+
+
 def video_capture():
-    #We call Haarcascade
-    #Face Cascade
+    
+    #We call Face Haarcascade
     faceCascade = cv2.CascadeClassifier("haarcascade_frontalface_alt2.xml")
+    #We call Eyes Haarcascade
+    eyesCascade = cv2.CascadeClassifier('haarcascade_eye.xml')
 
-
+    
+    #Container of inclinaison_head
+    LISTE = []
+    
     video_capture = cv2.VideoCapture(0)
 
     first_frame = None
     
     while True:
 
-
         _, frame = video_capture.read()
+        frame = cv2.resize(frame, (600, 600))
+        
         grayscale_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
+        drawing, first_frame = hand_function(frame, grayscale_frame,
+                                             first_frame, faceCascade)
 
-        drawing, first_frame = hand_function(frame, grayscale_frame, first_frame, faceCascade)
-
-
-        windows = {"dazdza":drawing, "yeux":frame, "tete":frame, "visage":frame}
+        windows = {"dazdza":drawing, "yeux":frame, "tete":frame}
  
         for nom, fenetre in windows.items():
             cv2.imshow(nom, fenetre)
+
+
+
+        if len(LISTE) < 5:
+            initialisation(grayscale_frame, faceCascade, eyesCascade, LISTE)
+            
+        else:
+            position1 = position_tete(frame, grayscale_frame, faceCascade, eyesCascade, LISTE)
+
+
+
+
+
+
+
+
+
 
 
 
@@ -49,9 +71,7 @@ def video_capture():
         if key == ord('q'):
             cv2.destroyAllWindows()
             break
-
-
-                    
+     
     video.release()
     cv2.destroyAllWindows()
         
@@ -78,3 +98,20 @@ def video_capture():
 
 if __name__ == "__main__":
     video_capture()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
