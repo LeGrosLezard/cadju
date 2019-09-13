2a2001ee0c5e1e9d95d1a682b529d6b4d075df3b
LeGrosLezard <53193299+LeGrosLezard@users.noreply.github.com>
2019-09-13 02:00:31 +0200

    Update essais.py
    bon mtn faut mettre un detecteur de mouvement ^^ jy arrive paaaaaaaaaaas

--- yeux/clignement/essais.py
+++ yeux/clignement/essais.py
@@ -3,131 +3,74 @@ import numpy as np
 from PIL import Image
 
 
-"""
-maintenant faut faire comme dans la video, soit test de
-récup le pourtoure soit mtn récupere
-la position de x avant, puis x et y mtn (l'oeil du coup va sauter !!!!)
-on dit si x ou y a bouger de temps et que mtn t'as ste position de l'oeil
-on peut replacer l'oeil via le saut de tete,
-faire justement le quadrant et repérer l'oeil !!!!!!
-et c mieux !
-"""
-
-
-
-
-
-
-
-
-#---------------------------------------------------------DETECTION FUNCTIONS
-def face_detection_config(gray, facecascade):
-    """Face detection with the following configuration:
-    the frame (here a crop of the frame does not gray),
-    the pyragmidal image, the minimum number of neighbors,
-    the size and the version of the haar"""
-    
-    faces = facecascade.detectMultiScale(
-        gray,
-        scaleFactor=1.3,
-        minNeighbors=1,
-        minSize=(30, 30),
-        flags=cv2.CASCADE_SCALE_IMAGE
-    )
-    return faces
-
-
-def eyes_detection_config(roi_gray, eyescascade):
-    """Eyes detection with the following configuration:
-    the frame (here a crop of the frame does not gray),
-    the pyragmidal image, the minimum number of neighbors,
-    the size and the version of the haar"""
-
-    eyes = eyescascade.detectMultiScale(
-        roi_gray,
-        scaleFactor=1.3,
-        minNeighbors=1,
-        minSize=(40, 40),
-        flags=cv2.CASCADE_SCALE_IMAGE
-    )
-    return eyes
-
-
-#---------------------------------------------------------IMAGE FUNCTIONS
-def eye_save(frame, eye, nom, counter, x, y, w, h):
-
-    crop = frame[y:y+h, x:x+w]
-    if eye == counter:
-        cv2.imwrite(nom, crop)
-
-
-
-def detection(frame, gray, facecascade, eyescascade):
-    
-    faces = face_detection_config(gray, facecascade)
-
-    for x1, y1, w1, h1 in faces:
-        roi_gray = gray[y1:y1+h1, x1:x1+w1]
-        roi_color = frame[y1:y1+h1, x1:x1+w1]
-        
-        eyes = eyes_detection_config(roi_gray, eyescascade) 
-
-        eye = 0
-        for x, y, w, h in eyes:
-            eye_save(roi_color, eye, "droite.jpg", 0, x, y, w, h)
-            eye_save(roi_color, eye, "gauche.jpg", 1, x, y, w, h)
-
-            eye += 1
-
-            cv2.rectangle(roi_color, (x, y), (x+w, y+h), (0, 0, 255), 2) 
-
-
-def resizer(image):
-    img = Image.open(image)
-    img = img.resize((500, 500), Image.ANTIALIAS).save(image)
-
-
-
-def circle(image):
-    
-    img = cv2.imread(image, 0)
-    gauss = cv2.GaussianBlur(img, (41, 41), 0)
-
-
-    try:
-        circles = cv2.HoughCircles(gauss, cv2.HOUGH_GRADIENT, 2, 100, param1=50,
-                                    param2=30, minRadius=40, maxRadius=55)
-
-        circles = np.round(circles[0, :]).astype("int")
-
-        for (x, y, r) in circles:
-            if x < 61:
-                pass
-            else:
-                cv2.circle(img, (x, y), r, (0, 0, 255), 1)
-                cv2.circle(img, (x, y), 2, (0, 0, 255), 3)
-                cv2.imshow("image", img)
-        
-    except:
-        pass
-
 
 
 eyescascade = cv2.CascadeClassifier('haar/haarcascade_eye.xml')
 facecascade = cv2.CascadeClassifier('haar/haarcascade_frontalface_alt2.xml')
 def video_capture():
 
-    video = cv2.VideoCapture(0)
 
+    video = cv2.VideoCapture(0)
+    liste = [[], [], [], []]
+    
     while(True):
 
-        ret, frame = video.read()        
+        ret, frame = video.read()
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        detection(frame, gray, facecascade, eyescascade)
-        resizer("droite.jpg")
-        circle("droite.jpg")
-       
+
+
+        def detection(gray, frame, facecascade, eyescascade, liste):
+
+            eyes = eyescascade.detectMultiScale(
+                gray,
+                scaleFactor=1.1,
+                minNeighbors=2,
+                minSize=(40, 40),
+                flags=cv2.CASCADE_SCALE_IMAGE
+            )
+            
+
+            c = 0
+            for x, y, w, h in eyes:
+
+                if c == 0:
+
+                    liste[0].append(x)
+                    liste[1].append(y)
+                    liste[2].append(x+w)
+                    liste[3].append(y+h)
+                    
+                    cv2.rectangle(frame, (x, y), (x+w, y+h), 3)
+                elif c == 1:
+                    cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 255, 0), 3)
+
+                c+=1
+
+                
+        if len(liste[0]) < 50:
+            detection(gray, frame, facecascade, eyescascade, liste)
+
+            
+        else:
+
+            def eyes(frame):
+
+                a = int(round(sum(liste[0]) / len(liste[0])))
+                b = int(round(sum(liste[1]) / len(liste[1])))
+                c = int(round(sum(liste[2]) / len(liste[2])))
+                d = int(round(sum(liste[3]) / len(liste[3])))
+
+                crop = frame[b:d, a:c]
+
+                cv2.imshow("frame1", crop)
+
+            eyes(frame)
+
+            
+
+                
         cv2.imshow("frame", frame)
+        
 
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
@@ -142,7 +85,5 @@ def video_capture():
 
 
 
-
-
-
-video_capture()
+if __name__ == "__main__":
+    video_capture()
