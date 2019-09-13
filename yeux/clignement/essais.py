fdf9e94bfa020dffe7464399daff9846ee7b6a85
LeGrosLezard <53193299+LeGrosLezard@users.noreply.github.com>
2019-09-13 16:30:35 +0200

    Update essais.py
    mtn faire le tresh automatic des fois ca marche des fois non chai pas

--- yeux/clignement/essais.py
+++ yeux/clignement/essais.py
@@ -23,11 +23,16 @@ def video_capture():
 
         def detection(gray, frame, facecascade, eyescascade, liste):
 
+
+
             eyes = eyescascade.detectMultiScale(
                 gray,
+                scaleFactor=1.3,
+                minNeighbors=4,
+                minSize=(30, 30),
+                maxSize=(50, 50),
             )
             
-
             c = 0
             for x, y, w, h in eyes:
 
@@ -51,12 +56,13 @@ def video_capture():
                 c+=1
 
                 
-        if len(liste[0]) < 50:
+        if len(liste[0]) < 30:
             detection(gray, frame, facecascade, eyescascade, liste)
 
             
         else:
 
+            #----------------------------------------
             def eyes(frame):
 
                 a = int(round(sum(liste[0]) / len(liste[0])))
@@ -69,32 +75,65 @@ def video_capture():
                 g = int(round(sum(liste1[2]) / len(liste1[2])))
                 h = int(round(sum(liste1[3]) / len(liste1[3])))
 
-                crop = frame[b+10:d-10, a+5:c-5]
-                crop1 = frame[f+10:h-10, e+5:g-5]
+                crop = frame[b+10:d, a:c]
+                crop1 = frame[f+10:h, e:g]
                 
                 return crop, crop1
 
-
-  
             crop, crop_g = eyes(frame)
+            #----------------------------------------
 
-        
+
+
+            #----------------------------------------
             gray = cv2.cvtColor(crop, cv2.COLOR_BGR2GRAY)
-            _, thresh = cv2.threshold(gray, 127, 255, 0)
-
-
+            gray = cv2.GaussianBlur(gray, (11, 11), 0)
+            _, thresh = cv2.threshold(gray, 70, 255, 0)
 
             gray1 = cv2.cvtColor(crop_g, cv2.COLOR_BGR2GRAY)
-            _, thresh1 = cv2.threshold(gray1, 127, 255, 0)
+            gray1 = cv2.GaussianBlur(gray1, (11, 11), 0)
+            _, thresh1 = cv2.threshold(gray1, 70, 255, 0)
+
+            #----------------------------------------
 
 
 
+            #----------------------------------------
             try:
-                contours, hierarchy = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+                contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
                 img = cv2.drawContours(crop, contours, 1, (0,255,0), 3)
 
+
+                ok = 10000000000000
+                for c in contours:
+                    if cv2.contourArea(c) < ok:
+                        ok = cv2.contourArea(c)
+                
+                for c in contours:
+                    if cv2.contourArea(c) == ok:
+                        M = cv2.moments(c)
+                        cX = int(M["m10"] / M["m00"])
+                        cY = int(M["m01"] / M["m00"])
+                        cv2.circle(crop, (cX, cY), 1, (0, 0, 255), 1)
+
+
+                    
                 contours1, hierarchy1 = cv2.findContours(thresh1,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
                 img1 = cv2.drawContours(crop_g, contours1, 1, (0,255,0), 3)
+
+
+                ok = 10000000000000
+                for c in contours:
+                    if cv2.contourArea(c) < ok:
+                        ok = cv2.contourArea(c)
+
+                for c in contours1:
+                    if cv2.contourArea(c) == ok:
+                        M = cv2.moments(c)
+                        cX = int(M["m10"] / M["m00"])
+                        cY = int(M["m01"] / M["m00"])
+                        cv2.circle(crop_g, (cX, cY), 1, (0, 0, 255), 1)
+
 
 
 
@@ -105,12 +144,13 @@ def video_capture():
     ##                cY = int(M["m01"] / M["m00"])
              
 
-                
-                cv2.imshow("p111oj11", thresh)
-                cv2.imshow("p111oj", thresh1)
             except:
                 pass
-            
+
+
+
+            cv2.imshow("p111oj11", thresh)
+            cv2.imshow("p111oj", thresh1)
         cv2.imshow("frame", frame)
         
 
