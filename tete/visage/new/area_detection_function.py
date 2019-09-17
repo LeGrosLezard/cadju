9afe26bc371282bd19c6868662190d528fea541a
LeGrosLezard <53193299+LeGrosLezard@users.noreply.github.com>
2019-09-17 14:37:43 +0200

    Update area_detection_function.py
    skin detecteur a refaire

--- tete/visage/new/area_detection_function.py
+++ tete/visage/new/area_detection_function.py
@@ -10,11 +10,6 @@ from collections import defaultdict
 
 
 def face_detection(faceCascade, gray, frame):
-    """Here we detecting the face thank to haarcascade.
-    We trying to detect the face from gray frame.
-    We make the pyramid scale to 1.3,
-    we define the minimum neighbour to 1, the minimum size to 60x40
-    and the flags for the old cascade."""
 
     #Face detector
     faces = faceCascade.detectMultiScale(
@@ -25,45 +20,14 @@ def face_detection(faceCascade, gray, frame):
         flags=cv2.CASCADE_SCALE_IMAGE
     )
 
-    """We crop, we only focus on the detection face,
-    for collect after, the main pixels and make our skin detector
-    of the face. We postulates skin face == skin arm for example
-    """
 
     #Points of the detecting face
     for x, y, w, h in faces:
-        cv2.rectangle(frame, (x, y), (x+w, y+h), 3)
-        frame_skin_detector = frame[y + 20:y+h - 20, x+20:x+w-20]
 
-        """This function serves us many times so we return
-        frame_skin_detector for focusing on the face,
-        we return also the points of the detection
-        """
-
+        frame_skin_detector = frame[y+50:y+h - 20, x+20:x+w-20]
         return frame_skin_detector, x, y, w, h
 
 
-
-def most_pixel(frame_skin_detector):
-    """Here we collect all pixels
-    from the face detection
-    and return the highter presence of pixel"""
-
-    dico = {}
-    img = Image.fromarray(frame_skin_detector)
-    for value in img.getdata():
-        if value in dico.keys():
-            dico[value] += 1
-        else:
-            dico[value] = 1
-
-    sorted_x = sorted(dico.items(),
-                      key=operator.itemgetter(1), reverse=True)
-
-    upper = sorted_x[0][0][0], sorted_x[0][0][1], sorted_x[0][0][2] + 40
-    lower = sorted_x[-1][0][0], sorted_x[-1][0][1], sorted_x[-1][0][2]
-
-    return upper, lower
 
 
 def append_list(liste, x1, y1, w1, h1, x2, y2, w2, h2, mode):
@@ -90,6 +54,59 @@ def append_list(liste, x1, y1, w1, h1, x2, y2, w2, h2, mode):
         liste[1].append(y1)
         liste[2].append(w1)
         liste[3].append(h1)
+
+
+
+def appending(tempe, patte, hear, mid, x, y, w, h):
+    """We initilise list, we append points of our area in function
+    of face detection"""
+
+    #temples
+    append_list(tempe,
+                x, y - 20, x - 40, y + 40,
+                x+w, y - 20, x+w+40, y + 40,
+                2)
+
+    #border forhead
+    append_list(patte,
+                x - 20, y - int(round(110 * 100 / h)), x + 30, y - int(round(50 * 100 / h)),
+                x+w-20, y - int(round(110 * 100 / h)), x+w+30, y-int(round(50 * 100 / h)),
+                2)
+    #hears
+    append_list(hear,
+                x - 40, y + 70, x, y + 150,
+                x+w, y + 70, x+w+40, y+150,
+                2)
+    #mid head
+    append_list(mid,
+                x + int(round(w/3)), y - int(round(150 * 100 / h)), x + int(round(w/3)) * 2, y - int(round(80 * 100 / h)),
+                "", "", "", "",
+                1)
+
+
+
+
+def most_pixel(frame_skin_detector):
+    """Here we collect all pixels
+    from the face detection
+    and return the highter presence of pixel"""
+
+    dico = {}
+    img = Image.fromarray(frame_skin_detector)
+    for value in img.getdata():
+        if value in dico.keys():
+            dico[value] += 1
+        else:
+            dico[value] = 1
+
+    sorted_x = sorted(dico.items(),
+                      key=operator.itemgetter(1), reverse=True)
+
+    upper = sorted_x[0][0][0], sorted_x[0][0][1], sorted_x[0][0][2] + 40
+    lower = sorted_x[-1][0][0], sorted_x[-1][0][1], sorted_x[-1][0][2]
+
+    return upper, lower
+
 
 
 
@@ -121,7 +138,7 @@ def out_list(liste, mode):
 
 def detections(frame, y1, yh1, x1, xw1,
                y2, yh2, x2, xw2,
-               message1, message2, messages):
+               message1, message2, MESSAGES):
     """Let's crop the differents are of our temples
     and try to see if white pixels are present.
     If we found white pixels, if want say
@@ -131,7 +148,7 @@ def detections(frame, y1, yh1, x1, xw1,
     area_two = frame[y2:yh2, x2:xw2]
 
 
-    def analysis_pixel_crop(area_one, message, messages):
+    def analysis_pixel_crop(area_one, message, MESSAGES):
         """This is a function of a function
         here we watch into the crop (into area temples)
         if there are 100 whites pixel
@@ -144,22 +161,49 @@ def detections(frame, y1, yh1, x1, xw1,
                     counter_pixel += 1
 
         if counter_pixel > 100:
-            displaying_message(messages, message)
-            messages.append(message)
+            displaying_message(MESSAGES, message)
+            MESSAGES.append(message)
 
-    analysis_pixel_crop(area_one, message1, messages)
-    analysis_pixel_crop(area_two, message2, messages)
+    analysis_pixel_crop(area_one, message1, MESSAGES)
+    analysis_pixel_crop(area_two, message2, MESSAGES)
 
     
 
     #NB tempe droite is right temples
     #NB tempe gauche is left temples
 
-def displaying_message(messages, message):
-    if messages[-1] == message or messages[-2] == message:
+
+def displaying_message(MESSAGES, message):
+    if MESSAGES[-1] == message or MESSAGES[-2] == message:
         pass
     else:
         print(message)
 
         
+def MOVEMENT_detector(frame, TEMPE, HEAR, PATTE, MID, MESSAGES, skinMask):
+    """We positionning our area from the mean of points
+    recuped from initialisation"""
 
+    #temples
+    y1, yh1, x1, xw1, y2, yh2, x2, xw2 = out_list(TEMPE, 2)
+    detections(skinMask, y1, yh1, x1, xw1, y2, yh2, x2, xw2,
+               "TEMPE droite", "TEMPE gauche", MESSAGES)
+    cv2.rectangle(frame, (x1, y1), (xw1, yh1), 3)
+    cv2.rectangle(frame, (x2, y2), (xw2, yh2), 3)
+    #HEAR
+    y1, yh1, x1, xw1, y2, yh2, x2, xw2 = out_list(HEAR, 2)
+    detections(skinMask, y1, yh1, x1, xw1, y2, yh2, x2, xw2,
+               "oreille droite", "oreille gauche", MESSAGES)
+    cv2.rectangle(frame, (x1, y1), (xw1, yh1), 3)
+    cv2.rectangle(frame, (x2, y2), (xw2, yh2), 3)
+    #border forehead
+    y1, yh1, x1, xw1, y2, yh2, x2, xw2 = out_list(PATTE, 2)
+    detections(skinMask, y1, yh1, x1, xw1, y2, yh2, x2, xw2,
+               "PATTE droite", "PATTE gauche", MESSAGES)
+    cv2.rectangle(frame, (x1, y1), (xw1, yh1), 3)
+    cv2.rectangle(frame, (x2, y2), (xw2, yh2), 3)
+    y1, yh1, x1, xw1 = out_list(MID, 1)
+    detections(skinMask, y1, yh1, x1, xw1, 0, 0, 0, 0,
+               "milieu de la tete", "", MESSAGES)
+    cv2.rectangle(frame, (x1, y1), (xw1, yh1), 3)
+    cv2.rectangle(frame, (x2, y2), (xw2, yh2), 3)
