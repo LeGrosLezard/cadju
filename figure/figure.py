afe20b776c244574394863d93ea1d77d8e982a48
LeGrosLezard <53193299+LeGrosLezard@users.noreply.github.com>
2019-09-21 15:52:49 +0200

    Update figure.py

--- figure/figure.py
+++ figure/figure.py
@@ -16,26 +16,33 @@ def face_detection(frame, gray, faceCascade):
         return x, y, w, h
 
 
-def recup_points(frame, x, y, w, h, liste):
+def recup_points(frame, x, y, w, h, liste_couleur):
+
     point = frame[y+100:y+101, x+50:x+51]
+
     point = point.tolist()
     point = point[0][0]
-    liste.append([point[0], point[1], point[2]])
-    print(liste)
+
+    liste_couleur.append([point[0], point[1], point[2]])
     cv2.rectangle(frame, (x+50, y+100), (x+51, y+101), (255, 0, 0), 5)
     
+    return  x+50, y+100, x+51, y+101
 
-def put_points(frame, liste, x, y, w, h):
 
-    
+def put_points(frame, liste_couleur, pts1, pts2, pts3, pts4,
+               x, y):
+    print("initialement", liste_couleur)
+    print("suposé", frame[y:y+1, x:x+1])
+    print("")
 
-   
-    cv2.rectangle(frame, (x+50, y+100), (x+51, y+101), (255, 0, 0), 5)
+    cv2.rectangle(frame, (pts1, pts2), (pts3, pts4), (255, 0, 0), 5)
+    cv2.rectangle(frame, (x, y), (x+1, y+1), (0, 0, 255), 5)
     
 
 
 def video_capture():
-    liste = []
+    liste_couleur = []
+
     X = 0
     Y = 0
     W = 0
@@ -49,31 +56,24 @@ def video_capture():
         frame = cv2.resize(frame, (800, 600))
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
+
         x, y, w, h = face_detection(frame, gray, faceCascade)
 
-        print(X,Y,W,H)
-        print(x, y, w, h)
 
-
-        if len(liste) < 1:
-            recup_points(frame, x, y, w, h, liste)
+        if len(liste_couleur) < 1:
+            pts1, pts2, pt3, pt4 = recup_points(frame, x, y, w, h,
+                                                liste_couleur)
 
         else:
+
             x, y, w, h = face_detection(frame, gray, faceCascade)
-            put_points(frame, liste, x, y, w, h)
+            put_points(frame, liste_couleur, pts1, pts2, pt3, pt4,
+                       x+50+(X-x), y+100+(Y-y))
         
-
-
-
-
-
-
-
-
 
         cv2.imshow("frame", frame)
 
-        key = cv2.waitKey(1) & 0xFF
+        key = cv2.waitKey(200) & 0xFF
         if key == ord('q'):
             cv2.destroyAllWindows()
             break
@@ -85,14 +85,3 @@ def video_capture():
 
 if __name__ == "__main__":
     video_capture()
-
-
-
-
-
-
-
-
-
-
-
