181fd1efa256d4cdca2ccd8d8495ec77536466e8
LeGrosLezard <53193299+LeGrosLezard@users.noreply.github.com>
2019-09-26 21:32:37 +0200

    Update ride.py
    mtn faut positionner les rides et le faire pour toute la gueule

--- figure/ride/ride.py
+++ figure/ride/ride.py
@@ -1,6 +1,6 @@
 import cv2
 import numpy as np
-
+from PIL import Image
 
 def adjust_gamma(image, gamma):
     """We add light to the video, we play with gamma"""
@@ -13,10 +13,14 @@ def adjust_gamma(image, gamma):
 
 
 
+
+
+
 facecascade = cv2.CascadeClassifier('../haar/haarcascade_frontalface_alt2.xml')
 eyescascade = cv2.CascadeClassifier('../haar/haarcascade_eye.xml')
 
 img = cv2.imread("WIN_20190925_13_48_04_Pro.jpg")
+img = adjust_gamma(img, 0.6)
 
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 faces = facecascade.detectMultiScale(
@@ -49,25 +53,82 @@ for x, y, w, h in faces:
         mean += x1+w1 / 2
     mean = int(mean /2)
     
+
+
     for x1, y1, w1, h1 in eyes:
         if x1+w1/2 < mean:
-            eyes_crop_l = crop[y1:y1+w1, x1:x1+w1]
-            edge_l = cv2.Canny(eyes_crop_l, 0, 0)
- 
-        else:
-            eyes_crop_r = crop[y1:y1+w1, x1:x1+w1]
-            edge_r = cv2.Canny(eyes_crop_r, 0, 0)
-    
 
-##
-##cv2.imshow("edge", edge)
-##cv2.imshow("image", img)
+            counter = 0
+            continuer = True
+            while continuer:
+            
+                eyes_crop_l = crop[y1:y1+w1, x1:x1+w1]
+                gray=cv2.cvtColor(eyes_crop_l, cv2.COLOR_BGR2GRAY)
+                
+                blur = cv2.GaussianBlur(gray, (11, 11), 0)
+                _, thresh = cv2.threshold(gray, 28, 255, 0)
 
 
-##cv2.imshow("eyes_crop_l", eyes_crop_l)
-##cv2.imshow("eyes_crop_r", eyes_crop_r)
-cv2.imshow("dzad", edge_l)
-cv2.imshow("eyes_vwvwcrop_r", edge_r)
+                contours, _ = cv2.findContours(thresh, cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+                for i in contours:
+
+                    if 1000 >= cv2.contourArea(i) >= 63.5:
+                        M = cv2.moments(i)
+                        cX = int(M["m10"] / M["m00"])
+                        cY = int(M["m01"] / M["m00"])
+
+                        cv2.circle(eyes_crop_l,(cX, cY), 2, (0,0,255), 5)
+
+                        continuer = False
+                        cv2.imshow("thresh", eyes_crop_l)
+                        cv2.waitKey(0)
+
+                counter += 1
+
+
+                edge_l = cv2.Canny(eyes_crop_l, 0, 0)
+                cv2.imwrite("eyes_crop_l.png", edge_l)
+                image_pil = Image.open("eyes_crop_l.png").resize((200,200), Image.ANTIALIAS)
+                image_pil.save("eyes_crop_l.png")
+     
+
+     
+            else:
+                eyes_crop_r = crop[y1:y1+w1, x1:x1+w1]
+                edge_r = cv2.Canny(eyes_crop_r, 40, 0)
+                cv2.imwrite("eyes_crop_r.png", edge_r)
+                image_pil = Image.open("eyes_crop_r.png").resize((200,200), Image.ANTIALIAS)
+                image_pil.save("eyes_crop_r.png")
+
+
+    edge = cv2.Canny(edge, 100, 255)
+
+
+
+
+
+
+img = cv2.imread("eyes_crop_l.png")
+a = img
+gray=cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+
+contours, _ = cv2.findContours(gray, cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+for i in contours:
+    cv2.drawContours(img, [i], 0, (0,255,0), 3)
+
+
+cv2.imshow("image", img)
+
+
+        
+
+
+
+##img2 = cv2.imread("eyes_crop_r.png")
+##cv2.imshow("image1", img2)
+
+
 
 
 
