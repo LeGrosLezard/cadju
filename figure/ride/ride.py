430f2c34b2a9a4f1e5fcc47bf4c3a40438226e9f
LeGrosLezard <53193299+LeGrosLezard@users.noreply.github.com>
2019-09-26 23:32:53 +0200

    Update ride.py
    bon mtn se renseigner sur les rides

--- figure/ride/ride.py
+++ figure/ride/ride.py
@@ -26,8 +26,12 @@ def make_line(thresh):
 facecascade = cv2.CascadeClassifier('../haar/haarcascade_frontalface_alt2.xml')
 eyescascade = cv2.CascadeClassifier('../haar/haarcascade_eye.xml')
 
-img = cv2.imread("WIN_20190925_13_48_04_Pro.jpg")
+img = cv2.imread("aa.jpg")
+
 img = adjust_gamma(img, 0.6)
+
+
+
 
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 faces = facecascade.detectMultiScale(
@@ -45,26 +49,31 @@ for x, y, w, h in faces:
     gray_crop = gray[y:y+h, x:x+w]
     edge = cv2.Canny(gray_crop, 40, 10)
 
+
+
     eyes = eyescascade.detectMultiScale(
         gray_crop,
         scaleFactor=1.3,
         minNeighbors=4,
         minSize=(30, 30),
-
         flags=cv2.CASCADE_SCALE_IMAGE
         )
 
-
+    
     mean = 0
     for x1, y1, w1, h1 in eyes:
         mean += x1+w1 / 2
     mean = int(mean /2)
     
 
+    for x1, y1, w1, h1 in eyes:
+        cv2.rectangle(crop, (x1, y1), (x1+w1, y1+h1), 3)
+    cv2.imshow("aaaaa", img)
+    cv2.waitKey(0)
 
     for x1, y1, w1, h1 in eyes:
         if x1+w1/2 < mean:
-
+            print("ici")
             counter = 0
             continuer = True
             while continuer:
@@ -84,10 +93,10 @@ for x, y, w, h in faces:
                         cX = int(M["m10"] / M["m00"])
                         cY = int(M["m01"] / M["m00"])
 
-                        cv2.circle(eyes_crop_l,(cX, cY), 2, (0,0,255), 5)
+                        cv2.circle(eyes_crop_l,(cX, cY), 2, (0,255,0), 5)
 
                         continuer = False
-
+                        
 
                 counter += 1
 
@@ -99,12 +108,53 @@ for x, y, w, h in faces:
      
 
      
-            else:
+        if x1+w1/2 > mean:
+            print("la")
+            
+
+            counter = 0
+            continuer = True
+            while continuer:
+
+                
                 eyes_crop_r = crop[y1:y1+w1, x1:x1+w1]
-                edge_r = cv2.Canny(eyes_crop_r, 40, 0)
-                cv2.imwrite("eyes_crop_r.png", edge_r)
-                image_pil = Image.open("eyes_crop_r.png").resize((200,200), Image.ANTIALIAS)
-                image_pil.save("eyes_crop_r.png")
+                gray=cv2.cvtColor(eyes_crop_r, cv2.COLOR_BGR2GRAY)
+                
+                blur = cv2.GaussianBlur(gray, (11, 11), 0)
+                _, thresh = cv2.threshold(gray, counter, 255, 0)
+
+
+                contours, _ = cv2.findContours(thresh, cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+
+                for i in contours:
+                    
+                    if 1000 >= cv2.contourArea(i) >= 60.0:
+                        M = cv2.moments(i)
+                        cX = int(M["m10"] / M["m00"])
+                        cY = int(M["m01"] / M["m00"])
+
+                        cv2.circle(eyes_crop_r,(cX, cY), 2, (0,0,255), 5)
+
+                        continuer = False
+                        cv2.imshow("aaaaa111", eyes_crop_r)
+                        cv2.waitKey(0)
+
+
+                        edge_l = cv2.Canny(eyes_crop_r, 40, 0)
+                        cv2.imwrite("eyes_crop_l.png", edge_l)
+                        image_pil = Image.open("eyes_crop_l.png").resize((200,200), Image.ANTIALIAS)
+                        image_pil.save("eyes_crop_l.png")
+
+                        
+                        cv2.imshow("edge_redge_redge_r", edge_l)
+                        cv2.waitKey(0)
+
+                counter += 1
+
+
+
+
+
 
 
 
