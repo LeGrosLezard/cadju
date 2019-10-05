48e1d928cd4219ecd9a0c1d39505fbba36f7fc76
LeGrosLezard <53193299+LeGrosLezard@users.noreply.github.com>
2019-10-05 20:50:56 +0200

    Update main.py
    bon ok les annotations
    par contre sté un tuto tf...
    faut les mettres dans un txt mtn

--- mains/code/main.py
+++ mains/code/main.py
@@ -1,3 +1,8 @@
+import cv2
+import numpy as np
+import csv
+import os
+
 import scipy
 import sklearn
 from sklearn.feature_extraction import image
@@ -8,37 +13,84 @@ import cv2
 import numpy as np
 
 
+from config import model
+from config import config
+from config import data
 
 
-points = scipy.io.loadmat("Buffy_1.mat")
-img = cv2.imread("Buffy_1.jpg")
+liste = os.listdir(r"C:\Users\jeanbaptiste\Desktop\cadju\mains\HAND\images\main_image\annotations")
+path = "../images/main_image/annotations/{}"
 
-coords = [[], []]
 
-for nb, i in enumerate(points["boxes"][0]):
-    for j in i[0]:
-        for k in j:
-            coords[nb].append(k[0].tolist())
- 
-
-for i in coords:
-    print(i)
+#les fichiers .mat
+for i in liste:
     print("")
+    print(i)
+
+    #ouverture fichier .mat
+    points = scipy.io.loadmat(path.format(str(i)))
+    coords = [[], []]
+
+    #recuperation des pts du .mat
+    for nb, i in enumerate(points["boxes"][0]):
+        for j in i[0]:
+            for k in j:
+                coords[nb].append(k[0].tolist())
+
+    #des fois y'a qu'un seul point qu'on definit par one
+    one = False
+    for lst in coords:
+        if lst == []:
+            one = True
+
+    #si pas one on recup les 2 pts
+    #sinon on stop
+    for nb, pts in enumerate(coords):
+        print(nb)
+        for p in pts:
+            print(p)
+        if one is True:
+            break
+        print("")
+
+
+    #faut définir tous les pts en txt.
 
 
 
-img[488,345] = 255, 255, 255
-img[461,348] = 255, 255, 255
-img[465,387] = 255, 255, 255
-img[492,384] = 255, 255, 255
 
 
-img[449,397] = 255, 255, 255
-img[462,426] = 255, 255, 255
-img[493,413] = 255, 255, 255
-img[480,384] = 255, 255, 255
 
-cv2.imshow("dazd", img)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
