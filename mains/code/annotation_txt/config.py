379cb6d9d7af0827e316fd0339f1085e8895802e
LeGrosLezard <53193299+LeGrosLezard@users.noreply.github.com>
2019-10-06 15:11:20 +0200

    Add files via upload

---
+++ mains/code/annotation_txt/config.py
@@ -0,0 +1,7 @@
+import os
+
+liste_mat = os.listdir(r"C:\Users\jeanbaptiste\Desktop\cadju\mains\HAND\images\main_image\annotations")
+path_mat = "../../images/main_image/annotations/{}"
+
+liste_picture = os.listdir(r"C:\Users\jeanbaptiste\Desktop\cadju\mains\HAND\images\main_image\images")
+path_picture = "../../images/main_image/images/{}"
---
+++ mains/code/annotation_txt/write_mat_to_txt.py
@@ -0,0 +1,121 @@
+import cv2
+import numpy as np
+import csv
+
+
+import scipy
+import sklearn
+from sklearn.feature_extraction import image
+from scipy.io import loadmat
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import cv2
+import numpy as np
+
+
+from config import liste_mat
+from config import path_mat
+from config import liste_picture
+from config import path_picture
+
+
+
+
+
+def recup_data_mat_file(i, path):
+
+    #ouverture fichier .mat
+    points = scipy.io.loadmat(path.format(str(i)))
+    coords = [[], []]
+
+    #recuperation des pts du .mat
+    for nb, i in enumerate(points["boxes"][0]):
+        for j in i[0]:
+            for k in j:
+                coords[nb].append(k[0].tolist())
+
+    return coords
+
+
+
+def define_numbers_point(coords):
+
+    #des fois y'a qu'un seul point qu'on definit par one
+    one = False
+    for lst in coords:
+        if lst == []:
+            one = True
+    return one
+
+
+def numbers_of_points(one, coords, path, i):
+
+    #si pas one on recup les 2 pts
+    #sinon on stop
+
+    first = True
+    
+    for nb, pts in enumerate(coords):
+        print(nb)
+
+        if nb == 1:
+            first = False
+
+        writting_annotation_to_picture(path, i, nb, first)
+        
+        for p in pts:
+            print(p[0])
+            
+            writting_points_into_txt(path, i, p[0])
+
+        if one is True:
+            break
+        print("")
+
+
+    #faut définir tous les pts en txt.
+
+
+def writting_annotation_to_picture(path, i, nb, first):
+
+    if first is True:
+        jump = nb
+    else:
+        jump = "\n" + str(nb)
+
+    i = str(i[:-4]) + ".txt"
+
+    print(path.format(i))
+
+    with open(path.format(i), "a") as file:
+        file.write(str(jump))
+        file.write(" ")
+        file.write("0")
+
+
+def writting_points_into_txt(path, i, p):
+
+    i = str(i[:-4]) + ".txt"
+    
+    with open(path.format(i), "a") as file:
+        file.write(" ")
+        file.write(str(p))
+
+
+
+
+def acess_to_list_path(liste, path):
+    #les fichiers .mat
+    for i in liste:
+        print("")
+
+
+        coords = recup_data_mat_file(i, path)
+        one = define_numbers_point(coords)
+        numbers_of_points(one, coords, path_picture, i)
+
+
+
+
+if __name__ == "__main__":
+    acess_to_list_path(liste_mat, path_mat)
